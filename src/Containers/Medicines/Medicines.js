54bf9317551f2bbef813b85a0eba1d6e50b5e96c
MaithilBoghara <bogharamaithil3@gmail.com>
2022-06-24 09:52:07 +0530

    v

--- src/Containers/Medicines/Medicines.js
+++ src/Containers/Medicines/Medicines.js
@@ -1,9 +1,72 @@
 import React from 'react';
 
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
 function Medicines(props) {
-    return (
-        <h1>This is a Medicines feild.</h1>
-    );
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        Open form Medicine
+      </Button>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Medicine</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+           Medicine Details
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Medicine Name"
+            type="text"
+            fullWidth
+            variant="standard"
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Price"
+            type="text"
+            fullWidth
+            variant="standard"
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Expiry Date"
+            type="text"
+            fullWidth
+            variant="standard"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleClose}>Agree</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
 }
+
 
 export default Medicines;
